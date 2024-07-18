f3f630c0af9e87cbbb8c7bdd5a8f705d97880b62
tjsp9830 <95339008+mcgr2010@users.noreply.github.com>
2024-07-18 12:31:36 +0900

    몬스터 구조체 추가

--- Day09_Git/Program.cs
+++ Day09_Git/Program.cs
@@ -9,11 +9,17 @@
             public int level;
         }
 
+        struct Monster
+        {
+            public string name;
+            public int hp;
+        }
+
 
         static void Main(string[] args)
         {
             Player player = new Player() { name = "김전사", level = 1 };
-
+            Monster slime = new Monster() { name = "슬라임", hp = 8 };
 
         }
 
